a856949e8dcfc8ebb782d5cb2f29687b96193998
warcgr01 <grzegorz.warchol@somfy.com>
2023-12-14 13:40:27 +0100

    wip

--- C++/main_interfejs.cpp
+++ C++/main_interfejs.cpp
@@ -1,70 +1,70 @@
 #include <iostream>
 #include <array>
 
-template <typename  T>
+
 class Liczby{
     public:
-        virtual T  &operator[](int i);
+        // virtual T  &operator[](int i);
         virtual int size()  = 0;
         virtual void show() = 0;
         virtual ~Liczby() {}
 };
 
-template <typename  T>
-class Liczby_int : public Liczby<T>
+
+class Liczby_int : public Liczby
 {
     public:
-        Liczby_int(std::initializer_list<T> licz): kontener{new T[licz.size()]}, ilosc{licz.size()}
+        Liczby_int(std::initializer_list<int> licz): kontener{new int[licz.size()]}, ilosc{licz.size()}
         {
             std::copy(licz.begin(), licz.end(), kontener);
         };
 
-        T  &operator[](int i) {return kontener[i];}
+        int  &operator[](int i) {return kontener[i];}
         int size() {return ilosc;}
         ~Liczby_int() {delete kontener;}
-
+        void show()   { 
+            for(int i = 0; i < ilosc; i++){
+             std::cout << kontener[i] <<" ";
+        }
+    }
     private:
-
-    T *kontener;
-    int ilosc;
+        int *kontener;
+        int ilosc;
 
 };
-template <typename  T>
-class Liczby_double : public Liczby<T>
+
+class Liczby_double : public Liczby
 {
     public:
-        Liczby_double(std::initializer_list<T> licz): kontener{new T[licz.size()]}, ilosc{licz.size()}
+        Liczby_double(std::initializer_list<double> licz): kontener{new double[licz.size()]}, ilosc{licz.size()}
         {
             std::copy(licz.begin(), licz.end(), kontener);
         };
 
-        T  &operator[](int i) {return  kontener[i];}
+        double  &operator[](int i) {return  kontener[i];}
         int size() {return ilosc;}
         ~Liczby_double() {delete kontener;}
 
+        void show()   { 
+            for(int i = 0; i < ilosc; i++){
+             std::cout << kontener[i] <<" ";
+            }
+        }
     private:
-
-    T *kontener;
-    int ilosc;
-
+        double *kontener;
+        int ilosc;
 };
 
 void show_liczby(Liczby &liczby)
 {
-    for(int i = 0; i < liczby.size(); i++){
-             std::cout << liczby[i] <<" ";
-    }
+   liczby.show();
    std::cout << std::endl;
    
 }
 int main()
 {
-
-    Liczby_int<int> licz = {5,7,6,3,1,4,9,7,5,2};
-    Liczby_double<double> licz_2 = {5.2, 7.5, 6.5, 3.5, 1.0, 4.1, 9.3, 7.4, 5.5, 2.6};
- 
+    Liczby_int licz = {5,7,6,3,1,4,9,7,5,2};
+    Liczby_double licz_2 = {5.2, 7.5, 6.5, 3.5, 1.0, 4.1, 9.3, 7.4, 5.5, 2.6};
     show_liczby(licz);
     show_liczby(licz_2);
-
-
 }
